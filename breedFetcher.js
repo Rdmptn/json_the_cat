adc7c05b5ddfce6f8f6afa8841e84c7dc80d143a
Cameron <cameron_uniac@hotmail.com>
2021-07-01 17:45:00 -0400

    Seperated function call into index.js and installed mocha for test functionality

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
-const breed =  process.argv.slice(2,3)
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log("Error: ", error);
-  } else if (data[0] === undefined) {
-    console.log("Breed not found!");
-  } else {
-    console.log("Description: " + data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error);
+    } else if (data[0] === undefined) {
+      callback(null, "Breed not found!");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
   
 
